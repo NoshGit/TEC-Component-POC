ca15afd311be664923b1c17b8b22799dca0b3d2a
Patel, Noshir N <noshir.patel@te.com>
2020-12-22 16:47:17 +0530

    CTA component made dynamic and document added

--- src/app/component/te-cta/te-cta.component.ts
+++ src/app/component/te-cta/te-cta.component.ts
@@ -8,9 +8,23 @@ import { Component, OnInit, Input } from '@angular/core';
 
 export class TeCtaComponent implements OnInit {
   @Input() ctaInfo: any;
+  ctaParams: any = {};
+  
   constructor() { }
 
   ngOnInit(): void {
+    if(this.ctaInfo){
+      this.ctaParams.classes = this.ctaInfo.classes || 'te-secondary-cta';
+      this.ctaParams.text = this.ctaInfo.text || 'CTA LABEL';
+      this.ctaParams.iconName = this.ctaInfo.iconName || '';
+    }else{
+      this.ctaParams = {
+        classes : 'te-secondary-cta',
+        text : 'CTA LABEL',
+        iconName : ''
+      };
+    }
+    
   }
 
 }
